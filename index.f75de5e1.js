b7c124da4672f6d14e092bffc928761c01756cac
MacBook Pro 13 <macbookpro13@MacBook-Pro-MacBook.local>
2024-07-01 12:03:01 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,34 @@
+"use strict";
+const body = document.getElementsByTagName("body")[0];
+const logo = document.getElementsByClassName("logo")[0];
+const div = document.createElement("div");
+const promise1 = async ()=>{
+    try {
+        div.classList.add("message");
+        div.textContent = "Promise was resolved!";
+    } catch  {
+        div.classList.add("message");
+        div.classList.add("error-message");
+        div.textContent = "Promise was rejected!";
+    } finally{
+        body.appendChild(div);
+    }
+};
+const promise2 = async ()=>{
+    try {
+        throw new Error();
+    } catch  {
+        setTimeout(()=>{
+            div.classList.add("message");
+            div.classList.add("error-message");
+            div.textContent = "Promise was rejected!";
+            body.appendChild(div);
+        }, 3000);
+    }
+};
+logo.addEventListener("click", ()=>{
+    promise1();
+    promise2();
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
